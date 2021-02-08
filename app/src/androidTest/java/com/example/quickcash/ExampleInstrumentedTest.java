23747cb8090d0e79e4d10f01f9abadcbb7f8aa22
bussey <dn334386@dal.com>
2021-02-08 16:59:16 -0400

    Added 1 expresso test BROKEN

--- app/src/androidTest/java/com/example/quickcash/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/quickcash/ExampleInstrumentedTest.java
@@ -1,13 +1,25 @@
 package com.example.quickcash;
 
 import android.content.Context;
+import android.content.Intent;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.example.quickcash.View.MainActivity;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -17,6 +29,9 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> myRule = new ActivityScenarioRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -24,4 +39,13 @@ public class ExampleInstrumentedTest {
         assertEquals("com.example.quickcash", appContext.getPackageName());
     }
 
+    @Test
+    public void checkIfSignUpUserNameIsEmpty() {
+        onView(withId(R.id.registerButton)).perform(click());
+        onView(withId(R.id.emailText)).perform(typeText("hello@dal.ca"));
+        onView(withId(R.id.usernameText)).perform(typeText(""));
+        onView(withId(R.id.passwordText)).perform(typeText("hello123"));
+        onView(withId(R.id.signupButton)).perform(click());
+        //assertNotNull(RegistrationViewModel.errorType.valueOf("invalidUserName"));
+    }
 }
