197c014b48714fd5803e3f845a47c3d2b026c670
Zeynep Oziskender <zy985939@dal.ca>
2021-04-03 18:16:50 -0300

    added dependencies and client ID

---
+++ app/src/main/java/com/example/quickcash/Config/Config.java
@@ -0,0 +1,6 @@
+package com.example.quickcash.Config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID ="AUrZWrSeEZhVMshIoMAqhvImhPyJamX_hk4djcVp_Ih7Od2HACb6Jm-gQSiT81LpEStsl7hcNa4GwsCa";
+
+}
