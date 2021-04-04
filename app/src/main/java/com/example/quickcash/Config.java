b4b9b8470ff2ab5eca5c1a51566ec7993ddf7791
jolmstead <jl260629@dal.ca>
2021-04-04 14:30:35 -0300

    XML for Pay Pal and payment business logic

--- app/src/main/java/com/example/quickcash/Config.java
+++ app/src/main/java/com/example/quickcash/Config.java
@@ -1,4 +1,4 @@
-package com.example.quickcash.Config;
+package com.example.quickcash;
 
 public class Config {
     public static final String PAYPAL_CLIENT_ID ="AUrZWrSeEZhVMshIoMAqhvImhPyJamX_hk4djcVp_Ih7Od2HACb6Jm-gQSiT81LpEStsl7hcNa4GwsCa";
